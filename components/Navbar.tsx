f61debc1f9c547620ec5238ddeede6bec8a1fd20
Ayush Kumar <aayushkumar1228@gmail.com>
2026-02-17 02:59:29 +0530

    add navbar component

---
+++ components/Navbar.tsx
@@ -0,0 +1,53 @@
+import React from 'react'
+import { Box } from 'lucide-react'
+import Button from './ui/Button';
+
+const Navbar = () => {
+
+    const isSignedIn = false;
+    const userName = 'ayush';
+
+    const handleAuthClick = async () => {
+
+    };
+
+    return (
+        <header className='navbar'>
+            <nav className='inner'>
+                <div className='left'>
+                    <div className='brand'>
+                        <Box className='logo' />
+                        <span className='name'>Lume Studio</span>
+                    </div>
+                    <ul className='links'>
+                        <a href="#">Product</a>
+                        <a href="#">Pricing</a>
+                        <a href="#">Community</a>
+                        <a href="#">Enterprise</a>
+                    </ul>
+                </div>
+                <div className="actions">
+                    {
+                        isSignedIn ? (
+                            <>
+                                <span className="greeting">
+                                    {userName ? `Hi, ${userName}` : 'Signed in'}
+                                </span>
+                                <Button size="sm" onClick={handleAuthClick} className="btn">
+                                    Log Out
+                                </Button>
+                            </>
+                        ) : (
+                            <>
+                                <Button onClick={handleAuthClick} size="sm" variant="ghost">Log In</Button>
+                                <a href='#upload' className='cta'>Get Started</a>
+                            </>
+                        )
+                    }
+                </div>
+            </nav>
+        </header>
+    )
+}
+
+export default Navbar
