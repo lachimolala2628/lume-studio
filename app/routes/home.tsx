b2e6ee3de7f4ae87dc57135cb33ac29c2b993dd7
Ayush Kumar <aayushkumar1228@gmail.com>
2026-02-18 01:28:11 +0530

    implement homepage UI

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,9 +1,11 @@
+import { ArrowRight, ArrowUpRight, Clock, Layers } from "lucide-react";
 import Navbar from "../../components/Navbar";
 import type { Route } from "./+types/home";
+import Button from "components/ui/Button";
 
 export function meta({ }: Route.MetaArgs) {
   return [
-    { title: "New React Router App" },
+    { title: "Lume Studio" },
     { name: "description", content: "Welcome to React Router!" },
   ];
 }
@@ -12,6 +14,82 @@ export default function Home() {
   return (
     <div className="home">
       <Navbar />
+
+      <section className="hero">
+        <div className="announce">
+          <div className="dot">
+            <div className="pulse"></div>
+          </div>
+
+          <p>Introducing Lume Studio 2.0</p>
+        </div>
+
+        <h1>Build beautiful spaces at the speed of thought with Lume Studio</h1>
+        <p className="subtitle">Lume Studio is an AI-first design environment that helps you visualize, render, and ship architectural projects faster than ever.</p>
+
+        <div className="actions">
+          <a href="#upload" className="cta">
+            Start Building <ArrowRight className="icon" />
+          </a>
+
+          <Button variant="outline" size="lg" className="demo">
+            Watch Demo
+          </Button>
+        </div>
+
+        <div id="upload" className="upload-shell">
+          <div className="grid-overlay" />
+          <div className="upload-card">
+            <div className="upload-head">
+              <div className="upload-icon">
+                <Layers className="icon" />
+              </div>
+              <h3>Upload your floor plans</h3>
+              <p>Supports JPG, PNG, formats upto 10MB</p>
+            </div>
+            <p>Upload Images</p>
+          </div>
+        </div>
+      </section>
+
+      <section className="projects">
+        <div className="section-inner">
+          <div className="section-head">
+            <div className="copy">
+              <h2>
+                Projects
+              </h2>
+              <p>
+                Your latest work and shared community projects, all in one place.
+              </p>
+            </div>
+          </div>
+          <div className="projects-grid">
+            <div className="project-card group">
+              <div className="preview">
+                <img src="https://roomify-mlhuk267-dfwu1i.puter.site/projects/1770803585402/rendered.png" alt="preivew image" />
+
+                <div className="badge">
+                  <span>Community</span>
+                </div>
+              </div>
+              <div className="card-body">
+                <div>
+                  <h3>Project Tokyo</h3>
+                  <div className="meta">
+                    <Clock size={12} />
+                    <span>{new Date('18.02.2026').toLocaleDateString()}</span>
+                    <span>By Aayu_2804</span>
+                  </div>
+                </div>
+                <div className="arrow">
+                  <ArrowUpRight size={18} />
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
     </div>
   )
 }
