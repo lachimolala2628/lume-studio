c61d6f251cf023f2bf9eb2a0a4b241e28c3d0378
Ayush Kumar <aayushkumar1228@gmail.com>
2026-02-23 02:07:43 +0530

    implement before/after image comparison slider component and file download feature

--- app/routes/visualizer.$id.tsx
+++ app/routes/visualizer.$id.tsx
@@ -3,6 +3,7 @@ import { generate3DView } from 'lib/ai.action';
 import { createProject, getProjectById } from 'lib/puter.action';
 import { Box, Download, RefreshCcw, Share2, X } from 'lucide-react';
 import React, { useEffect, useRef, useState } from 'react'
+import { ReactCompareSlider, ReactCompareSliderImage } from 'react-compare-slider';
 import { useOutletContext } from 'react-router';
 import { useNavigate, useParams } from 'react-router';
 
@@ -20,6 +21,17 @@ const visualizerId = () => {
     const [currentImage, setCurrentImage] = useState<string | null>(null);
 
     const handleBack = () => navigate('/');
+
+    const handleExport = () => {
+        if(!currentImage) return;
+
+        const link = document.createElement('a');
+        link.href = currentImage;
+        link.download = `lume_studio-${id || 'design'}.png`;
+        document.body.appendChild(link);
+        link.click();
+        document.body.removeChild(link);
+    };
 
     const runGeneration = async (item: DesignItem) => {
         if (!id || !item.sourceImage) return;
@@ -127,7 +139,7 @@ const visualizerId = () => {
                         <div className='panel-actions'>
                             <Button
                                 size='sm'
-                                onClick={() => { }}
+                                onClick={handleExport}
                                 className='export'
                                 disabled={!currentImage}
                             >
@@ -135,7 +147,6 @@ const visualizerId = () => {
                             </Button>
                             <Button
                                 size='sm'
-                                onClick={() => { }}
                                 className='share'
                             >
                                 <Share2 className='w-4 h-4 mr-2' /> share
@@ -168,7 +179,42 @@ const visualizerId = () => {
                             </div>
                         )}
                     </div>
+                </div>
 
+                <div className="panel compare">
+                    <div className="panel-header">
+                        <div className="panel-meta">
+                            <p>Comparsion</p>
+                            <h3>Before and After</h3>
+                        </div>
+                        <div className="hint">
+                            Drag to compare
+                        </div>
+                    </div>
+                    <div className="compare-stage">
+                        {
+                            project?.sourceImage && currentImage ? (
+                                <ReactCompareSlider
+                                    defaultValue={50}
+                                    style={{ width: '100%', height: 'auto' }}
+                                    itemOne={
+                                        <ReactCompareSliderImage src={project?.sourceImage!} alt="before" className="compare-img" />
+                                    }
+                                    itemTwo={
+                                        <ReactCompareSliderImage src={(currentImage || project?.renderedImage)!} alt="after" className="compare-img" />
+                                    }
+                                />
+                            ) : (
+                                <div className="compare-fallback">
+                                    {
+                                        project?.sourceImage && (
+                                            <img src={project.sourceImage} alt='Before' className='compare-img' />
+                                        )
+                                    }
+                                </div>
+                            )
+                        }
+                    </div>
                 </div>
             </section>
         </div>
